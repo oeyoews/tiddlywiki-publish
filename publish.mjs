e9162d60d6ac8dbb6f4092b6fe0608a33ebb1d6c
oeyoews <jyao4783@gmail.com>
2025-04-14 23:25:51 +0800

    fix: remove dep info

--- publish.mjs
+++ publish.mjs
@@ -4,26 +4,42 @@ import { tiddlywiki } from './tiddlywiki.mjs';
 import fs from 'fs';
 import path from 'path';
 
+const buildArgs = [
+  '--verbose',
+  '--output',
+  '.tiddlywiki',
+  '--render',
+  '$:/plugins/tiddlywiki/tiddlyweb/save/offline',
+  'index.html',
+  'text/plain',
+  '',
+  'publishFilter',
+  '-[tag[private]] -[is[draft]]',
+];
+
 // 获取命令行参数
 const args = process.argv.slice(2);
 const tiddlerDirIndex = args.findIndex((arg) => arg === '--tiddlers-directory');
 const tiddlerDir =
   tiddlerDirIndex !== -1 ? args[tiddlerDirIndex + 1] : undefined;
 
-const command = ['--build'];
+// const command = ['--build'];
 const __dirname = import.meta.dirname;
 if (tiddlerDir && fs.existsSync(path.join(__dirname, tiddlerDir))) {
-  command.unshift(tiddlerDir);
+  buildArgs.unshift(tiddlerDir);
   // 如果tiddlerDir !== tiddler, 并且tiddlerDir 不存在tiddlywiki.info, 就把tiddlywiki.info 复制到tiddlerDir
-  if (tiddlerDir !== '.') {
-    const tiddlywikiInfoPath = `${tiddlerDir}/tiddlywiki.info`;
-    if (!fs.existsSync(tiddlywikiInfoPath)) {
-      fs.copyFileSync(path.join(__dirname, './actions/tiddlywiki.info'), tiddlywikiInfoPath);
-    }
-  }
+  // if (tiddlerDir !== '.') {
+  //   const tiddlywikiInfoPath = `${tiddlerDir}/tiddlywiki.info`;
+  //   if (!fs.existsSync(tiddlywikiInfoPath)) {
+  //     fs.copyFileSync(
+  //       path.join(__dirname, './actions/tiddlywiki.info'),
+  //       tiddlywikiInfoPath
+  //     );
+  //   }
+  // }
 } else {
-  command.unshift('.');
+  buildArgs.unshift('.');
 }
 
-await tiddlywiki(command);
-console.log('publish success', command);
+await tiddlywiki(buildArgs);
+console.log('publish success', buildArgs);

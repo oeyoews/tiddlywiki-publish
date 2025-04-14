194336018a2a2c9f73c07416f27179c02b8fa111
oeyoews <jyao4783@gmail.com>
2025-04-14 21:52:13 +0800

    fix: update .gitignore to exclude $__StoryList files and enhance publish.mjs for better directory handling

--- publish.mjs
+++ publish.mjs
@@ -1,25 +1,27 @@
-import { tiddlywiki } from './tiddlywiki.mjs'
-import fs from 'fs'
+import { tiddlywiki } from './tiddlywiki.mjs';
+import fs from 'fs';
 import path from 'path';
 
 // 获取命令行参数
 const args = process.argv.slice(2);
-const tiddlerDirIndex = args.findIndex(arg => arg === '--tiddler-directory');
-const tiddlerDir = tiddlerDirIndex !== -1 ? args[tiddlerDirIndex + 1] : undefined;
+const tiddlerDirIndex = args.findIndex((arg) => arg === '--tiddlers-directory');
+const tiddlerDir =
+  tiddlerDirIndex !== -1 ? args[tiddlerDirIndex + 1] : undefined;
 
 const command = ['--build'];
-if (tiddlerDir && fs.existsSync(path.resolve(tiddlerDir))) {
-    command.unshift(tiddlerDir);
-// 如果tiddlerDir !== tiddler, 并且tiddlerDir 不存在tiddlywiki.info, 就把tiddlywiki.info 复制到tiddlerDir
-    if (tiddlerDir !== 'tiddler') {
-        const tiddlywikiInfoPath = `${tiddlerDir}/tiddlywiki.info`;
-        if (!fs.existsSync(tiddlywikiInfoPath)) {
-            fs.copyFileSync('tiddlywiki.info', tiddlywikiInfoPath);
-        }
+const __dirname = import.meta.dirname;
+if (tiddlerDir && fs.existsSync(path.join(__dirname, tiddlerDir))) {
+  command.unshift(tiddlerDir);
+  // 如果tiddlerDir !== tiddler, 并且tiddlerDir 不存在tiddlywiki.info, 就把tiddlywiki.info 复制到tiddlerDir
+  if (tiddlerDir !== 'tiddler') {
+    const tiddlywikiInfoPath = `${tiddlerDir}/tiddlywiki.info`;
+    if (!fs.existsSync(tiddlywikiInfoPath)) {
+      fs.copyFileSync('tiddlywiki.info', tiddlywikiInfoPath);
     }
+  }
 } else {
-	command.unshift('.');
+  command.unshift('.');
 }
 
-await tiddlywiki(command)
-console.log('publish success', command)
+await tiddlywiki(command);
+console.log('publish success');

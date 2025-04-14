de6d1009635b4b08b6f408077ffeff246987dd78
oeyoews <jyao4783@gmail.com>
2025-04-14 21:08:02 +0800

    fix: update build

--- publish.mjs
+++ publish.mjs
@@ -4,14 +4,22 @@ import { tiddlywiki } from './tiddlywiki.mjs'
 const args = process.argv.slice(2);
 const tiddlerDirIndex = args.findIndex(arg => arg === '--tiddler-directory');
 const tiddlerDir = tiddlerDirIndex !== -1 ? args[tiddlerDirIndex + 1] : undefined;
-console.log(tiddlerDir, 'tiddler dir')
 
 const command = ['--build'];
 if (tiddlerDir) {
     command.unshift(tiddlerDir);
+// 如果tiddlerDir !== tiddler, 并且tiddlerDir 不存在tiddlywiki.info, 就把tiddlywiki.info 复制到tiddlerDir
+    if (tiddlerDir !== 'tiddler') {
+        const tiddlywikiInfoPath = `${tiddlerDir}/tiddlywiki.info`;
+        if (!fs.existsSync(tiddlywikiInfoPath)) {
+            fs.copyFileSync('tiddlywiki.info', tiddlywikiInfoPath);
+        }
+    }
 } else {
-	command.unshift( './tiddlers');
+	command.unshift('.');
 }
+
+
 
 await tiddlywiki(command)
 console.log('publish success')

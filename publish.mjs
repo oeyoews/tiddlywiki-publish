6ae8837602c2bece36496ff2929630c76962216a
oeyoews <jyao4783@gmail.com>
2025-04-14 22:57:39 +0800

    fix: update

--- publish.mjs
+++ publish.mjs
@@ -4,6 +4,7 @@ import { tiddlywiki } from './tiddlywiki.mjs';
 import fs from 'fs';
 import path from 'path';
 
+console.log('build')
 // 获取命令行参数
 const args = process.argv.slice(2);
 const tiddlerDirIndex = args.findIndex((arg) => arg === '--tiddlers-directory');
@@ -12,13 +13,14 @@ const tiddlerDir =
 
 const command = ['--build'];
 const __dirname = import.meta.dirname;
+console.log('build3')
 if (tiddlerDir && fs.existsSync(path.join(__dirname, tiddlerDir))) {
   command.unshift(tiddlerDir);
   // 如果tiddlerDir !== tiddler, 并且tiddlerDir 不存在tiddlywiki.info, 就把tiddlywiki.info 复制到tiddlerDir
   if (tiddlerDir !== 'tiddler') {
     const tiddlywikiInfoPath = `${tiddlerDir}/tiddlywiki.info`;
     if (!fs.existsSync(tiddlywikiInfoPath)) {
-      fs.copyFileSync(path.join(import.meta.dirname, './actions/tiddlywiki.info'), tiddlywikiInfoPath);
+      fs.copyFileSync(path.join(__dirname, './actions/tiddlywiki.info'), tiddlywikiInfoPath);
     }
   }
 } else {

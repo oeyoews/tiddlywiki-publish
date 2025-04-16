d19bd1456fb1929dd4ae150841c478165af9b01d
oeyoews <jyao4783@gmail.com>
2025-04-16 16:09:28 +0800

    fix: update argument parsing in publish script and improve site-title handling

--- publish.mjs
+++ publish.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import minimist from './minimist.js';
 import { tiddlywiki } from './tiddlywiki.mjs';
 import fs from 'fs';
 import path from 'path';
@@ -18,48 +19,31 @@ const buildArgs = [
 ];
 
 // 获取命令行参数
-const args = process.argv.slice(2);
-const tiddlerDirIndex = args.findIndex((arg) => arg === '--tiddlers-directory');
-const tiddlerDir =
-  tiddlerDirIndex !== -1 ? args[tiddlerDirIndex + 1] : undefined;
+const _args = process.argv.slice(2);
 
-const defaultHomeTiddler = '--default-home-tiddlers';
-const defaultTiddlersIndex = args.findIndex((arg) =>
-  arg.startsWith(defaultHomeTiddler)
-);
+const args = minimist(_args, {
+  string: ['tiddlers-directory', 'default-home-tiddlers', 'site-title'],
+  unknown: false,
+  alias: {
+    tiddlerDir: 'tiddlers-directory',
+    defaultHomeTiddler: 'default-home-tiddlers',
+    siteTitle: 'site-title',
+  },
+});
 
-const defaultTiddlersValue = args[defaultTiddlersIndex]?.slice(
-  defaultHomeTiddler.length + 1
-);
-
-const siteTitleArg = '--site-title';
-const siteTitleArgIndex = args.findIndex((arg) => arg.startsWith(siteTitleArg));
-
-const siteTitleArgValue = args[siteTitleArgIndex]?.slice(
-  siteTitleArg.length + 1
-);
+console.log(args)
 
 // const command = ['--build'];
 const __dirname = import.meta.dirname;
-if (tiddlerDir) {
+if (args.tiddlerDir) {
   const tiddlerPath = fs.existsSync(
-    path.join(__dirname, tiddlerDir, 'tiddlers')
+    path.join(__dirname, args.tiddlerDir, 'tiddlers')
   );
   if (tiddlerPath) {
-    buildArgs.unshift(tiddlerDir);
+    buildArgs.unshift(args.tiddlerDir);
   } else {
     console.error('❌', tiddlerPath, 'is not existing');
   }
-  // 如果tiddlerDir !== tiddler, 并且tiddlerDir 不存在tiddlywiki.info, 就把tiddlywiki.info 复制到tiddlerDir
-  // if (tiddlerDir !== '.') {
-  //   const tiddlywikiInfoPath = `${tiddlerDir}/tiddlywiki.info`;
-  //   if (!fs.existsSync(tiddlywikiInfoPath)) {
-  //     fs.copyFileSync(
-  //       path.join(__dirname, './actions/tiddlywiki.info'),
-  //       tiddlywikiInfoPath
-  //     );
-  //   }
-  // }
 } else {
   buildArgs.unshift('.');
 }
@@ -74,24 +58,14 @@ const siteTitleTiddler = {
   text: '',
 };
 
-// if (defaultTiddlersValue) {
-//   defaultTiddlers.text = defaultTiddlersValue
-//     .split(',')
-//     .map((item) => {
-//       const trimmed = item.trim();
-//       return trimmed.includes(' ') ? `[[${trimmed}]]` : trimmed;
-//     })
-//     .join(' ');
-// }
-
 const preloadTiddlers = [];
-if (defaultTiddlersValue) {
-  defaultTiddlers.text = defaultTiddlersValue;
-  preloadTiddlers.push(defaultHomeTiddler);
+if (args.defaultHomeTiddler) {
+  defaultTiddlers.text = args.defaultHomeTiddler;
+  preloadTiddlers.push(defaultTiddlers);
 }
 
-if (siteTitleArgValue) {
-  siteTitleTiddler.text = siteTitleArgValue;
+if (args.siteTitle) {
+  siteTitleTiddler.text = args.siteTitle;
   preloadTiddlers.push(siteTitleTiddler);
 }
 
